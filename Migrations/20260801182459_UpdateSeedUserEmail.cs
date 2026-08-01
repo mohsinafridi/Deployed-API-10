b677b80b6a8f4a41ffd03d1c58a686e4b183b243
Mohsin <mohsin.afridi91@gmail.com>
2026-08-01 22:29:01 +0400

    changes

---
+++ Migrations/20260801182459_UpdateSeedUserEmail.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedUserEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "mohsin.azam@gmail.ae");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "mohsin.azam@adports.ae");
+        }
+    }
+}
